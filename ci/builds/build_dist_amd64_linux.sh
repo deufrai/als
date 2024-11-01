ab6b008b83f1b48ca1eecbbeaa3d668fbd3d4735
Frédéric (deufrai) CORNU <fcornu@wardsback.org>
2024-11-01 05:43:02 +0100

    build amd64_linux on an ubuntu 22.04 builder

--- ci/builds/build_dist_amd64_linux.sh
+++ ci/builds/build_dist_amd64_linux.sh
@@ -2,18 +2,17 @@ set -e
 
 artifact_name="als-${ALS_VERSION_STRING}.run"
 
-pyenv local 3.6.9
 python -m venv venv
-. ./venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+source venv/bin/activate
+pip install --upgrade pip wheel
+pip install -r ci/builds/build_dist_amd64_linux_req.txt
 
 python setup.py develop
 
 echo "version = \"${ALS_VERSION_STRING}\"" > src/als/version.py
 
 echo "Building package ${artifact_name} ..."
-pyinstaller -F -n "${artifact_name}" --windowed src/als/main.py
+pyinstaller -F -n "${artifact_name}" --windowed --hidden-import='scipy._lib.array_api_compat.numpy.fft' src/als/main.py --hiddenimport=scipy.special._special_ufuncs
 mv dist/${artifact_name} .
 echo "Build of package ${artifact_name} completed OK."
 
