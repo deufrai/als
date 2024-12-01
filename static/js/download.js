8013808ee8f0592e33b410ff531efd24899c6e7a
Frédéric CORNU <fcornu@wardsback.org>
2024-12-01 06:51:32 +0100

    fix redirection from download button to install instructions according to LANG
    impact of site default LANG switch from FR to EN

--- static/js/download.js
+++ static/js/download.js
@@ -2,23 +2,23 @@ function downloadAndOpenInstructions(platform) {
     var urls = {
         'windows': {
             'download': 'https://github.com/deufrai/als/releases/download/v0.7-beta6/als-v0.7-beta6.exe',
-            'instructions': window.location.pathname.includes('/en/') ? '/en/docs/v0.7/installation/windows-install/' : '/docs/v0.7/installation/windows-install/'
+            'instructions': window.location.pathname.includes('/fr/') ? '/fr/docs/v0.7/installation/windows-install/' : '/docs/v0.7/installation/windows-install/'
         },
         'mac-intel': {
             'download': 'https://github.com/deufrai/als/releases/download/v0.7-beta6/ALS-v0.7-beta6-amd64.dmg',
-            'instructions': window.location.pathname.includes('/en/') ? '/en/docs/v0.7/installation/mac-intel-install/' : '/docs/v0.7/installation/mac-intel-install/'
+            'instructions': window.location.pathname.includes('/fr/') ? '/fr/docs/v0.7/installation/mac-intel-install/' : '/docs/v0.7/installation/mac-intel-install/'
         },
         'mac-arm': {
             'download': 'https://github.com/deufrai/als/releases/download/v0.7-beta6/ALS-v0.7-beta6-arm64.dmg',
-            'instructions': window.location.pathname.includes('/en/') ? '/en/docs/v0.7/installation/mac-arm-install/' : '/docs/v0.7/installation/mac-arm-install/'
+            'instructions': window.location.pathname.includes('/fr/') ? '/fr/docs/v0.7/installation/mac-arm-install/' : '/docs/v0.7/installation/mac-arm-install/'
         },
         'linux': {
             'download': 'https://github.com/deufrai/als/releases/download/v0.7-beta6/als-v0.7-beta6.run',
-            'instructions': window.location.pathname.includes('/en/') ? '/en/docs/v0.7/installation/linux-install/' : '/docs/v0.7/installation/linux-install/'
+            'instructions': window.location.pathname.includes('/fr/') ? '/fr/docs/v0.7/installation/linux-install/' : '/docs/v0.7/installation/linux-install/'
         },
         'raspberry-pi': {
             'download': 'https://github.com/deufrai/als/releases/download/v0.7-beta6/als-v0.7-beta6.tgz',
-            'instructions': window.location.pathname.includes('/en/') ? '/en/docs/v0.7/installation/raspberry-pi-install/' : '/docs/v0.7/installation/raspberry-pi-install/'
+            'instructions': window.location.pathname.includes('/fr/') ? '/fr/docs/v0.7/installation/raspberry-pi-install/' : '/docs/v0.7/installation/raspberry-pi-install/'
         }
     };
 
