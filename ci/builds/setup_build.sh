50ca3c60ee5815017ee4c355b3c48e223a665f79
Frédéric CORNU <fcornu@wardsback.org>
2026-05-31 03:26:31 +0200

    ignore non-version tags for build numbering
    Filter tags used by the build setup job so only version-shaped tags become ALS_VERSION_STRING.
    
    prevents windows build from failing on non version-shaped tags used in various CI testing scenarios

--- ci/builds/setup_build.sh
+++ ci/builds/setup_build.sh
@@ -6,24 +6,28 @@ set -e
 #
 ###########################################################################
 
+version_tag_pattern='^v[0-9]+(\.[0-9]+)*$'
 tags=$(git tag --contains HEAD)
+version_tags=$(echo "${tags}" | grep -E "${version_tag_pattern}" || true)
 
-if [ -z "${tags}" ]
+if [ -z "${version_tags}" ]
 then
-  tag_count=0
+  version_tag_count=0
 else
-  tag_count=$(echo "${tags}" | wc -l)
+  version_tag_count=$(echo "${version_tags}" | wc -l)
 fi
 
-if [ ${tag_count} -gt 1 ]
+if [ ${version_tag_count} -gt 1 ]
 then
-    echo "More that one tag exist on HEAD. Cancelling ..."
+    echo "More that one version tag exists on HEAD. Cancelling ..."
     exit 1
 fi
 
-if [ $tag_count -eq 1 ]
+if [ ${version_tag_count} -eq 1 ]
 then
-  ALS_VERSION_STRING=$(git tag --contains HEAD)
+  # in here we are sure the version_tags var only contains one tag, so we can safely assign it to version_tag
+  version_tag=${version_tags}
+  ALS_VERSION_STRING=${version_tag}
 else
   ALS_VERSION_STRING=$(grep version src/als/version.py | cut -d'"' -f2)-$(git rev-parse --short HEAD)
   ALS_VERSION_STRING="${ALS_VERSION_STRING}-bld${CI_PIPELINE_ID}"
@@ -47,4 +51,3 @@ echo "ALS_VERSION_STRING=${ALS_VERSION_STRING}" > .dotenv
 echo '######### env file dump START'
 cat .dotenv
 echo '######### env file dump END'
-
