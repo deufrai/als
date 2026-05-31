e414353492b6e6c0fdc3af5e3344b8fcb49c5ad3
Frédéric CORNU <fcornu@wardsback.org>
2026-05-31 22:27:28 +0200

    make notebook cleanup script also remove metadatas

--- analyzer/tools/clear_nb_outputs.sh
+++ analyzer/tools/clear_nb_outputs.sh
@@ -1,2 +1,12 @@
-jupyter nbconvert --clear-output notebooks/*.ipynb
+#!/usr/bin/env bash
 
+set -euo pipefail
+
+cd "$(dirname "$0")/.."
+
+jupyter nbconvert \
+  --clear-output \
+  --ClearMetadataPreprocessor.enabled=True \
+  --ClearMetadataPreprocessor.clear_cell_metadata=True \
+  --ClearMetadataPreprocessor.clear_notebook_metadata=True \
+  notebooks/*.ipynb
